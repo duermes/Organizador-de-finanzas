d57438e730c3f2da8f4fac2b4b91b816f2e5f69f
Lorena Kim <iloveposhitos@gmail.com>
2023-07-12 21:54:37 -0500

    añadí una nota en el main

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,11 @@ public class Main {
     public static void main(String[] args) {
         new Gui();
 
+        /*
+        NOTA: lo que está entre "//" es un código del Joptionpane que SÍ funciona, pero
+        la interfaz me pareció algo fea así que decidí hacerlo a mano es la clase Gui. Si lo quitas
+        de notas el código funciona (por si lo quieres probar)
+         */
         /*JOptionPane panel = new JOptionPane();
         String titulo = "Calculadora de finanzas personales";
         String nombre = panel.showInputDialog(null,"Ingresa tu nombre: ",titulo, JOptionPane.PLAIN_MESSAGE);
