246c61f88e02436dbd9e32b516c66687c2a2f780
Lorena Kim <iloveposhitos@gmail.com>
2023-07-12 21:39:42 -0500

    prueba

--- src/Gui.java
+++ src/Gui.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicComboBoxRenderer;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,7 +8,9 @@ public class Gui implements ActionListener {
     private JFrame frame;
     private JPanel panel;
     private JLabel label;
+    private JLabel label2
     private JButton button;
+    //hola
 
     public Gui() {
 
@@ -17,11 +20,13 @@ public class Gui implements ActionListener {
 
         panel = new JPanel();
         label = new JLabel("Bienvenido");
+        label2 = new JLabel("Hola que tal");
 
         panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 100, 300));
         panel.setLayout(new GridLayout(0, 1));
         panel.add(label);
         panel.add(button);
+        panel.add(label2);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
