69054897c9d27b08b274a98bcc1e51b2220fb222
Lorena Kim <iloveposhitos@gmail.com>
2023-07-14 23:07:39 -0500

    Prototipo de GUI terminado

--- src/GuiDemo.java
+++ src/GuiDemo.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GuiDemo {
 
-    private JLabel Titulo;
+    private JLabel titulo;
     private JPanel infoPanel;
     private JTextField tfGastos;
     private JTextField tfAhorrar;
@@ -14,6 +17,7 @@ public class GuiDemo {
     private JLabel presupuesto;
     private JLabel ahorrar;
     private JLabel gastos;
+    private GuiResumen gui2;
 
     public GuiDemo() {
         JFrame frame = new JFrame("Calculadora para finanzas personales");
@@ -22,7 +26,44 @@ public class GuiDemo {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
+        nombre.setForeground(Color.WHITE);
+        presupuesto.setForeground(Color.WHITE);
+        ahorrar.setForeground(Color.WHITE);
+        gastos.setForeground(Color.WHITE);
+        titulo.setForeground(Color.WHITE);
+
+
+        enviarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gui2 = new GuiResumen();
+                JFrame frame2 = new JFrame("Menú de resumen Calculadora para finanzas personales");
+                frame2.setContentPane(gui2.getPanel1());
+                frame2.setSize(700,500);
+                frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(false);
+                frame2.setVisible(true);
+
+
+                gui2.getRestante().setForeground(Color.WHITE);
+                gui2.getBalance().setForeground(Color.WHITE);
+                gui2.getGastos().setForeground(Color.WHITE);
+                gui2.getList1().setForeground(Color.WHITE);
+                gui2.getBalance1().setForeground(Color.WHITE);
+                gui2.getGastos1().setForeground(Color.WHITE);
+                gui2.getRestante1().setForeground(Color.WHITE);
+                gui2.getAñadirGastos().setForeground(Color.WHITE);
+                gui2.getTitulo().setForeground(Color.WHITE);
+                gui2.getAceptarButton().setForeground(Color.BLACK);
+
+            }
+        });
     }
 
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
--- src/GuiResumen.java
+++ src/GuiResumen.java
@@ -3,7 +3,52 @@ import javax.swing.*;
 public class GuiResumen {
 
     private JPanel panel1;
-    private JLabel textArea1;
-    private JLabel textArea2;
-    private JLabel textArea3;
+    private JLabel balance;
+    private JLabel gastos;
+    private JLabel restante;
+    private JPanel hola;
+    private JList list1;
+    private JButton aceptarButton;
+    private JLabel balance1;
+    private JLabel gastos1;
+    private JLabel restante1;
+    private JLabel titulo;
+    private JLabel añadirGastos;
+
+
+    public JLabel getTitulo() {
+        return titulo;
+    }
+    public JLabel getAñadirGastos() {
+        return añadirGastos;
+    }
+    public JPanel getPanel1() {
+        return panel1;
+    }
+    public JLabel getBalance() {
+        return balance;
+    }
+    public JLabel getGastos() {
+        return gastos;
+    }
+    public JLabel getRestante() {
+        return restante;
+    }
+    public JLabel getBalance1() {
+        return balance1;
+    }
+    public JLabel getGastos1() {
+        return gastos1;
+    }
+    public JLabel getRestante1() {
+        return restante1;
+    }
+    public JList getList1() {
+        return list1;
+    }
+    public JButton getAceptarButton() {
+        return aceptarButton;
+    }
+
+
 }
